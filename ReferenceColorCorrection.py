22b4987505f95ba4b31483416873d04313c06110
Vladislav Polyukhov <vlad.polyukhov@gmail.com>
2020-05-06 11:17:52 +0300

    Update ReferenceColorCorrection.py

--- ReferenceColorCorrection.py
+++ ReferenceColorCorrection.py
@@ -15,19 +15,16 @@ right_color = [255, 255, 255]  # ice.jpg (white color RedGreenBlue)
 #  right_color = [170, 152, 169]  # people.jpg (grey)
 
 division = [0, 0, 0]
-division[0] = right_color[0] / wrong_color[0]
-division[1] = right_color[1] / wrong_color[1]
-division[2] = right_color[2] / wrong_color[2]
-
-# Checking for 'wrapping'
-np.putmask(image[:, :, 0], image[:, :, 0] * division[0] > 255, 255)
-np.putmask(image[:, :, 1], image[:, :, 1] * division[1] > 255, 255)
-np.putmask(image[:, :, 2], image[:, :, 2] * division[2] > 255, 255)
 
 image = image.astype('float64')
-np.putmask(image[:, :, 0], image[:, :, 0] * division[0] <= 255, image[:, :, 0] * division[0])
-np.putmask(image[:, :, 1], image[:, :, 1] * division[1] <= 255, image[:, :, 1] * division[1])
-np.putmask(image[:, :, 2], image[:, :, 2] * division[2] <= 255, image[:, :, 2] * division[2])
+
+for i in range(3):
+    division[i] = right_color[i] / wrong_color[i]
+    # Checking for 'wrapping'
+    np.putmask(image[:, :, i], image[:, :, i] * division[i] > 255, 255)
+    # Major changes
+    np.putmask(image[:, :, i], image[:, :, i] * division[i] <= 255, image[:, :, i] * division[i])
+
 image = image.astype('uint8')
 
 print("--- %s seconds ---" % (time.time() - start_time))
